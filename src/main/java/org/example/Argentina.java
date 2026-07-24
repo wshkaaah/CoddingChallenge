8a4fc9f6d485497abfe1efad626f986625232b0a
wshkaah <sasanmalikov@gmail.com>
2026-07-24 21:23:40 +0900

    argentina bye bye

--- src/main/java/org/example/Argentina.java
+++ src/main/java/org/example/Argentina.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Argentina {
     public static void main(String[] args) {
-        System.out.println("Hello Argentina");
+        System.out.println("bye bye");
     }
 }
