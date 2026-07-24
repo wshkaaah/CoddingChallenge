9764252b4426bb371d966e90524d63e9f4ae4930
wshkaah <sasanmalikov@gmail.com>
2026-07-24 21:24:16 +0900

    Spain champ

--- src/main/java/org/example/Argentina.java
+++ src/main/java/org/example/Argentina.java
@@ -2,7 +2,6 @@ package org.example;
 
 public class Argentina {
     public static void main(String[] args) {
-        System.out.println("bye bye");
-        System.out.println("Argentina is not champion");
+        System.out.println("Spain is champion!!!");
     }
 }
